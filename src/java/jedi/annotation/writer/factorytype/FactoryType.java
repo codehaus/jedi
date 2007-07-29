b707256f6223a300d3db43b7e46f71af247060e5
channingwalton <channingwalton@37252faf-9d31-0410-a7b4-842fbc3ebc0e>
2007-07-29 20:08:14 +0000

    added some suppress warnings

--- src/java/jedi/annotation/writer/factorytype/FactoryType.java
+++ src/java/jedi/annotation/writer/factorytype/FactoryType.java
@@ -7,18 +7,23 @@ import jedi.annotation.writer.JavaWriter;
 import com.sun.mirror.declaration.TypeDeclaration;
 
 public abstract class FactoryType {
-    public String getQualifiedTypeName(TypeDeclaration typeDeclaration) {
-        return getPackage(typeDeclaration) + "." + getSimpleTypeName(typeDeclaration);
-    }
+	public String getQualifiedTypeName(TypeDeclaration typeDeclaration) {
+		return getPackage(typeDeclaration) + "." + getSimpleTypeName(typeDeclaration);
+	}
 
-    private String getPackage(TypeDeclaration typeDeclaration) {
-        return typeDeclaration.getPackage().getQualifiedName();
-    }
-    
-    public void writeClassHeader(PrintWriter writer, TypeDeclaration typeDeclaration) {}
+	private String getPackage(TypeDeclaration typeDeclaration) {
+		return typeDeclaration.getPackage().getQualifiedName();
+	}
 
-    public abstract String getSimpleTypeName(TypeDeclaration typeDeclaration);
-    public abstract String getTypeDeclaration(TypeDeclaration typeDeclaration);
-    public abstract void writeMethodBody(ClosureFragmentWriter writer, JavaWriter javaWriter);
-    public abstract void writeMethodModifiers(PrintWriter writer);
+	@SuppressWarnings("unused")
+	public void writeClassHeader(PrintWriter writer, TypeDeclaration typeDeclaration) {
+	}
+
+	public abstract String getSimpleTypeName(TypeDeclaration typeDeclaration);
+
+	public abstract String getTypeDeclaration(TypeDeclaration typeDeclaration);
+
+	public abstract void writeMethodBody(ClosureFragmentWriter writer, JavaWriter javaWriter);
+
+	public abstract void writeMethodModifiers(PrintWriter writer);
 }

8ebf7ef509176365c85d61faaa5fd815d36b2ade
lancewalton <lancewalton@37252faf-9d31-0410-a7b4-842fbc3ebc0e>
2008-09-07 11:35:01 +0000

    Fixed the Options example

--- trunk/examples/options/src/jedi/example/CommandExample.java
+++ trunk/examples/options/src/jedi/example/CommandExample.java
@@ -2,6 +2,7 @@ package jedi.example;
 
 import static jedi.option.Options.Some;
 import jedi.annotation.JediCommand;
+import jedi.option.None;
 import jedi.option.Option;
 
 public class CommandExample {
@@ -12,12 +13,14 @@ public class CommandExample {
 	}
 
 	@JediCommand
-	public void doY(String arg) {
+	public void doY(None<String> arg) {
 		
 	}
 	
 	public static void main(String[] args) {
+		CommandExample example = new CommandExample();
+		
 		Option<String> thing = Some("thing");
-		thing.match(CommandExampleStaticClosureFactory.doXCommand(), CommandExampleStaticClosureFactory.doYCommand());
+		thing.match(CommandExampleStaticClosureFactory.doXProxyCommand(example), CommandExampleStaticClosureFactory.doYProxyCommand(example));
 	}
 }

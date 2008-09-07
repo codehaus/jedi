b5666e7c913125eb0c1d750b200c89b2ead8de47
channingwalton <channingwalton@37252faf-9d31-0410-a7b4-842fbc3ebc0e>
2008-09-07 13:16:27 +0000

    added docs

--- trunk/src/java/jedi/option/Options.java
+++ trunk/src/java/jedi/option/Options.java
@@ -7,14 +7,28 @@ import java.util.Map;
  */
 public final class Options {
 
+	/**
+	 * Factory method for Some.
+	 * @return Some(value)
+	 */
 	public static <T> Option<T> Some(T value) {
 		return new Some<T>(value);
 	}
 	
+	/**
+	 * Factory method for None.
+	 * @return None
+	 */
 	public static <T> Option<T> None() {
 		return new None<T>();
 	}
 
+	/**
+	 * Get an Option from a Map.
+	 * @param map the map to retrieve a value from
+	 * @param key the key to use
+	 * @return None if the map did not contain a value for <code>key</code>, Some(value) if the map did contain the value.
+	 */
 	public static <K, V> Option<V> get(Map<K, V> map, K key) {
 		return map.containsKey(key) ? Some(map.get(key)) : Options.<V>None();
 	}

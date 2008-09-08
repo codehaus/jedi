21d14daabacbcc0c74a55c677387e2578fc2c30b
channingwalton <channingwalton@37252faf-9d31-0410-a7b4-842fbc3ebc0e>
2008-09-08 19:32:58 +0000

    improved docs

--- src/java/jedi/option/OptionMatcher.java
+++ src/java/jedi/option/OptionMatcher.java
@@ -1,5 +1,8 @@
 package jedi.option;
 
+/**
+ * Receiver for Some or None in {@link Option#match(OptionMatcher)}.
+ */
 public interface OptionMatcher<T> {
 	void caseSome(T value);
 	void caseNone(None<T> none);

f05fe9a6254283344337413d4b6f2d8914c71ac4
channingwalton <channingwalton@37252faf-9d31-0410-a7b4-842fbc3ebc0e>
2011-03-25 21:35:02 +0000

    code fix

--- examples/options/src/main/java/jedi/example/OptionExample.java
+++ examples/options/src/main/java/jedi/example/OptionExample.java
@@ -1,14 +1,16 @@
 package jedi.example;
 
-import static jedi.option.Options.some;
 import jedi.option.Option;
 import jedi.option.OptionMatcher;
+
+import static jedi.option.Options.some;
 
 public class OptionExample {
 
 	public static void main(String[] args) {
 		Option<String> x = some("foo");
-		
+
+        // Matching using a visitor
 		x.match(new OptionMatcher<String>() {
 			public void caseNone() {
 				System.out.println("oops, shouldn't be here");
